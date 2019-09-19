bfb0d8ac7a0cbc019d27278cf66fb9ac714a2baf
yuanqn <1546022688@qq.com>
2019-09-19 22:57:43 +0800

    zuul 网关

---
+++ spring-cloud-learn-zuul/src/main/java/com/yuanqinnan/spring/cloud/learn/zuul/ZuulApplication.java
@@ -0,0 +1,20 @@
+package com.yuanqinnan.spring.cloud.learn.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Description: spring-cloud-learn
+ * <p>
+ * Created by yuanqn on 2019/9/19 22:27
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
