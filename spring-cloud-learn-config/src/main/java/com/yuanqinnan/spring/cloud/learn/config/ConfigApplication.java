aa7e1b03f6713446c061a9e10d30fd131e4f6d1c
yuanqn <1546022688@qq.com>
2019-09-22 23:20:16 +0800

    zuul 网关与分布式配置中心

---
+++ spring-cloud-learn-config/src/main/java/com/yuanqinnan/spring/cloud/learn/config/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.yuanqinnan.spring.cloud.learn.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @des:
+ * @author: yuanqinnan
+ * @date 2019/9/22
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
