3d2ff4e9f198bcc587bf872f4f4a97587c05da7f
yuanqn <1546022688@qq.com>
2019-09-23 23:42:51 +0800

    zipkin

---
+++ spring-cloud-learn-zipkin/src/main/java/com/yuanqinnan/spring/cloud/learn/zipkin/ZipKinApplication.java
@@ -0,0 +1,21 @@
+package com.yuanqinnan.spring.cloud.learn.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * @des:
+ * @author: yuanqinnan
+ * @date 2019/9/23
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class, args);
+    }
+}
